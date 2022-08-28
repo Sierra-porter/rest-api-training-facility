d81e7a5788118c738d2a2ab6dce38aba9df1300a
artemijafanasev <artemiys117@mail.ru>
2022-08-28 17:28:51 +0300

    Initial commit

---
+++ src/main/java/com/example/restapitrainingfacility/RestApiTrainingFacilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapitrainingfacility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTrainingFacilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTrainingFacilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restapitrainingfacility/RestApiTrainingFacilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapitrainingfacility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTrainingFacilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
