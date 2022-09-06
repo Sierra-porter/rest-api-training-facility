505306f7964384174c47c9f240db4a04fc5820a6
artemijafanasev <artemiys117@mail.ru>
2022-09-06 13:51:54 +0300

    add code to Author.java Book.java

--- src/main/java/com/example/restapitrainingfacility/entity/Author.java
+++ src/main/java/com/example/restapitrainingfacility/entity/Author.java
@@ -1,4 +1,79 @@
 package com.example.restapitrainingfacility.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+import java.util.*;
+
+
+@Entity
+@Table(name = "author")
 public class Author {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "midlename")
+    private String midlename;
+
+    @Column(name = "birthdate")
+    private String birthdate;
+
+    @Column(name = "deathdate")
+    private String deathdate;
+
+    @Column(name = "description")
+    private String description;
+
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    }, mappedBy = "authors")
+
+    @JsonIgnore
+    private Set<Book> books = new HashSet<>();
+
+    public Author(){
+    }
+
+    public Author(String firstname, String lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    public Author(String firstname, String lastname, String midlename,
+                  String birthdate, String deathdate,String description){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.midlename = midlename;
+        this.birthdate = birthdate;
+        this.deathdate = deathdate;
+        this.description = description;
+    }
+
+    public UUID getId() {return id;}
+    public String getFirstname() {return firstname;}
+    public String getMidlename() {return midlename;}
+    public String getBirthdate() {return birthdate;}
+    public String getDeathdate() {return deathdate;}
+    public String getDescription() {return description;}
+    public Set<Book> getBooks() {return books;}
+    public String getLastname() {return lastname;}
+    public void setId(UUID id) {this.id = id;}
+    public void setFirstname(String firstname) {this.firstname = firstname;}
+    public void setLastname(String lastname) {this.lastname = lastname;}
+    public void setMidlename(String midlename) {this.midlename = midlename;}
+    public void setBirthdate(String birthdate) {this.birthdate = birthdate;}
+    public void setDeathdate(String deathdate) {this.deathdate = deathdate;}
+    public void setDescription(String description) {this.description = description;}
+    public void setBooks(Set<Book> books) {this.books = books;}
 }
--- src/main/java/com/example/restapitrainingfacility/entity/Book.java
+++ src/main/java/com/example/restapitrainingfacility/entity/Book.java
@@ -1,4 +1,123 @@
 package com.example.restapitrainingfacility.entity;
 
+import com.example.restapitrainingfacility.entity.Author;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "book")
 public class Book {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "year")
+    private String year;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "pages")
+    private String pages;
+
+    @Column(name = "publisher")
+    private String publisher;
+
+    @Column(name = "file")
+    private String file;
+
+    @Column(name = "added")
+    private String added;
+
+    @Column(name = "modified")
+    private String modified;
+
+    @Column(name = "file_added")
+    private String fileadded;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(name = "book_has_author", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new HashSet<>();
+
+    public Book(){
+    }
+
+    public Book(String title, String year, String genre, String pages, String publisher, String file, String added, String modified, String fileadded) {
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.pages = pages;
+        this.publisher = publisher;
+
+        setAdded();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public String getAdded() {
+        return added;
+    }
+
+    public String getModified() {
+        return modified;
+    }
+
+    public String getFileadded() {
+        return fileadded;
+    }
+
+    public void addAuthor(Author author) {
+        this.authors.add(author);
+        author.getBooks().add(this);
+    }
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+    public void removeAuthor(Author author) {
+        this.authors.remove(author);
+    }
+    public void addFile(String file) {
+        this.file = file;
+    }
+    private void setAdded() {this.added = this.modified = new Date();}
+
+    public void setModified() {
+        this.modified = new Date();
+    }
+
+    public void setFileAdded() {
+        this.fileAdded = new Date();
+    }
+
+
 }
