1b3239e297e5ee18a4a7c5efb2048f4cf1ff236d
artemijafanasev <artemiys117@mail.ru>
2022-09-07 08:40:01 +0300

    add ResponseMessage.java

--- src/main/java/com/example/restapitrainingfacility/helper/ResponseMessage.java
+++ src/main/java/com/example/restapitrainingfacility/helper/ResponseMessage.java
@@ -1,4 +1,13 @@
 package com.example.restapitrainingfacility.helper;
 
 public class ResponseMessage {
+
+    private String message;
+
+    public ResponseMessage(String message) {this.message = message; }
+
+    public String getMessage(){ return message; }
+
+    public void setMessage(String message){ this.message = message; }
+
 }
