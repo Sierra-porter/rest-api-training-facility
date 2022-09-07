75fe857f6811faead252761c0b198333af2a72af
artemijafanasev <artemiys117@mail.ru>
2022-09-07 08:46:20 +0300

    make interface FileStoraggeService.java

--- src/main/java/com/example/restapitrainingfacility/storageservice/FileStoraggeService.java
+++ src/main/java/com/example/restapitrainingfacility/storageservice/FileStoraggeService.java
@@ -1,4 +1,13 @@
 package com.example.restapitrainingfacility.storageservice;
 
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
 public interface FileStoraggeService {
+
+    public void init();
+
+    public void save(MultipartFile file);
+
+    public Resource load(String fileName);
 }
