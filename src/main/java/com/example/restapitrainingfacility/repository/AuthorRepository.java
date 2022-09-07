f251b1c02da14f52ad44e87f62bdd2e465803772
artemijafanasev <artemiys117@mail.ru>
2022-09-07 08:35:08 +0300

    add code to AuthorRepository.java and BookRepository.java

--- src/main/java/com/example/restapitrainingfacility/repository/AuthorRepository.java
+++ src/main/java/com/example/restapitrainingfacility/repository/AuthorRepository.java
@@ -1,4 +1,25 @@
 package com.example.restapitrainingfacility.repository;
 
-public interface AuthorRepository {
+import java.util.List;
+import java.util.UUID;
+
+import com.example.restapitrainingfacility.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, UUID>{
+    List<Author> findAuthorsByBooksId(UUID bookId);
+
+    List<Author> findByFirstnameAndLastname(String firstname, String lastname);
+
+    List<Author> findByFirstname(String firstname);
+
+    List<Author> findByLastname(String lastname);
+
+    List<Author> findByBirthDate(String birthDate);
+
+    List<Author> findByDeathDate(String deathDate);
+
+    List<Author> findByDescriptionContaining(String description);
+
+
 }
--- src/main/java/com/example/restapitrainingfacility/repository/BookRepository.java
+++ src/main/java/com/example/restapitrainingfacility/repository/BookRepository.java
@@ -1,4 +1,22 @@
 package com.example.restapitrainingfacility.repository;
 
-public interface BookRepository {
+import java.util.List;
+import java.util.UUID;
+
+import com.example.restapitrainingfacility.entity.Author;
+import com.example.restapitrainingfacility.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, UUID> {
+    List<Book> findBooksByAuthorsId(UUID authorId);
+
+    List<Book> findByYear(Short year);
+
+    List<Book> findByGenre(String genre);
+
+    List<Book> findByPublisher(String publisher);
+
+    List<Book> findByTitleContaining(String title);
+
+    List<Book> findByAuthorsContains(Author author);
 }
